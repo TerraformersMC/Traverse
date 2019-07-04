1022be56f4cfdd02ea6b5795ab3f392636d39bd2
Prospector <prospectordev@gmail.com>
2019-07-04 13:38:28 -0700

    Update mappings + game version

--- src/main/java/io/github/terraformersmc/traverse/block/TraverseBlocks.java
+++ src/main/java/io/github/terraformersmc/traverse/block/TraverseBlocks.java
@@ -23,47 +23,47 @@ import java.util.Map;
 
 public class TraverseBlocks {
 
-    private static final Map<Identifier, BlockItem> ITEMS = new HashMap<>();
-    private static final Map<Identifier, Block> BLOCKS = new HashMap<>();
+	private static final Map<Identifier, BlockItem> ITEMS = new HashMap<>();
+	private static final Map<Identifier, Block> BLOCKS = new HashMap<>();
 
-    public static final Block RED_AUTUMNAL_LEAVES = add("red_autumnal_leaves", new TerraformLeavesBlock(), ItemGroup.DECORATIONS);
-    public static final Block RED_AUTUMNAL_SAPLING = add("red_autumnal_sapling", new TerraformSaplingBlock(new TerraformSaplingGenerator(() -> new TerraformOakTreeFeature(DefaultFeatureConfig::deserialize, true, Blocks.DARK_OAK_LOG.getDefaultState(), RED_AUTUMNAL_LEAVES.getDefaultState()))), ItemGroup.DECORATIONS);
-    public static final Block BROWN_AUTUMNAL_LEAVES = add("brown_autumnal_leaves", new TerraformLeavesBlock(), ItemGroup.DECORATIONS);
-    public static final Block BROWN_AUTUMNAL_SAPLING = add("brown_autumnal_sapling", new TerraformSaplingBlock(new TerraformSaplingGenerator(() -> new TerraformOakTreeFeature(DefaultFeatureConfig::deserialize, true, 5, Blocks.OAK_LOG.getDefaultState(), BROWN_AUTUMNAL_LEAVES.getDefaultState()))), ItemGroup.DECORATIONS);
-    public static final Block ORANGE_AUTUMNAL_LEAVES = add("orange_autumnal_leaves", new TerraformLeavesBlock(), ItemGroup.DECORATIONS);
-    public static final Block ORANGE_AUTUMNAL_SAPLING = add("orange_autumnal_sapling", new TerraformSaplingBlock(new TerraformSaplingGenerator(() -> new TerraformOakTreeFeature(DefaultFeatureConfig::deserialize, true, Blocks.OAK_LOG.getDefaultState(), ORANGE_AUTUMNAL_LEAVES.getDefaultState()))), ItemGroup.DECORATIONS);
-    public static final Block YELLOW_AUTUMNAL_LEAVES = add("yellow_autumnal_leaves", new TerraformLeavesBlock(), ItemGroup.DECORATIONS);
-    public static final Block YELLOW_AUTUMNAL_SAPLING = add("yellow_autumnal_sapling", new TerraformSaplingBlock(new TerraformSaplingGenerator(() -> new TerraformOakTreeFeature(DefaultFeatureConfig::deserialize, true, 6, Blocks.BIRCH_LOG.getDefaultState(), YELLOW_AUTUMNAL_LEAVES.getDefaultState()))), ItemGroup.DECORATIONS);
+	public static final Block RED_AUTUMNAL_LEAVES = add("red_autumnal_leaves", new TerraformLeavesBlock(), ItemGroup.DECORATIONS);
+	public static final Block RED_AUTUMNAL_SAPLING = add("red_autumnal_sapling", new TerraformSaplingBlock(new TerraformSaplingGenerator(() -> new TerraformOakTreeFeature(DefaultFeatureConfig::deserialize, true, Blocks.DARK_OAK_LOG.getDefaultState(), RED_AUTUMNAL_LEAVES.getDefaultState()))), ItemGroup.DECORATIONS);
+	public static final Block BROWN_AUTUMNAL_LEAVES = add("brown_autumnal_leaves", new TerraformLeavesBlock(), ItemGroup.DECORATIONS);
+	public static final Block BROWN_AUTUMNAL_SAPLING = add("brown_autumnal_sapling", new TerraformSaplingBlock(new TerraformSaplingGenerator(() -> new TerraformOakTreeFeature(DefaultFeatureConfig::deserialize, true, 5, Blocks.OAK_LOG.getDefaultState(), BROWN_AUTUMNAL_LEAVES.getDefaultState()))), ItemGroup.DECORATIONS);
+	public static final Block ORANGE_AUTUMNAL_LEAVES = add("orange_autumnal_leaves", new TerraformLeavesBlock(), ItemGroup.DECORATIONS);
+	public static final Block ORANGE_AUTUMNAL_SAPLING = add("orange_autumnal_sapling", new TerraformSaplingBlock(new TerraformSaplingGenerator(() -> new TerraformOakTreeFeature(DefaultFeatureConfig::deserialize, true, Blocks.OAK_LOG.getDefaultState(), ORANGE_AUTUMNAL_LEAVES.getDefaultState()))), ItemGroup.DECORATIONS);
+	public static final Block YELLOW_AUTUMNAL_LEAVES = add("yellow_autumnal_leaves", new TerraformLeavesBlock(), ItemGroup.DECORATIONS);
+	public static final Block YELLOW_AUTUMNAL_SAPLING = add("yellow_autumnal_sapling", new TerraformSaplingBlock(new TerraformSaplingGenerator(() -> new TerraformOakTreeFeature(DefaultFeatureConfig::deserialize, true, 6, Blocks.BIRCH_LOG.getDefaultState(), YELLOW_AUTUMNAL_LEAVES.getDefaultState()))), ItemGroup.DECORATIONS);
 
-    public static final Block FIR_LOG = add("fir_log", new LogBlock(MaterialColor.BROWN, FabricBlockSettings.copy(Blocks.DARK_OAK_LOG).build()), ItemGroup.DECORATIONS);
-    public static final Block FIR_LEAVES = add("fir_leaves", new TerraformLeavesBlock(), ItemGroup.DECORATIONS);
-    public static final Block FIR_SAPLING = add("fir_sapling", new TerraformSaplingBlock(new TerraformSaplingGenerator(() -> new FirTreeFeature(DefaultFeatureConfig::deserialize, true))), ItemGroup.DECORATIONS);
+	public static final Block FIR_LOG = add("fir_log", new LogBlock(MaterialColor.BROWN, FabricBlockSettings.copy(Blocks.DARK_OAK_LOG).build()), ItemGroup.DECORATIONS);
+	public static final Block FIR_LEAVES = add("fir_leaves", new TerraformLeavesBlock(), ItemGroup.DECORATIONS);
+	public static final Block FIR_SAPLING = add("fir_sapling", new TerraformSaplingBlock(new TerraformSaplingGenerator(() -> new FirTreeFeature(DefaultFeatureConfig::deserialize, true))), ItemGroup.DECORATIONS);
 
-    private static <B extends Block> B add(String name, B block, ItemGroup tab) {
-        return add(name, block, new BlockItem(block, new Item.Settings().itemGroup(tab)));
-    }
+	private static <B extends Block> B add(String name, B block, ItemGroup tab) {
+		return add(name, block, new BlockItem(block, new Item.Settings().group(tab)));
+	}
 
-    private static <B extends Block> B add(String name, B block, BlockItem item) {
-        add(name, block);
-        if (item != null) {
-            item.registerBlockItemMap(Item.BLOCK_ITEM_MAP, item);
-            ITEMS.put(new Identifier(Traverse.MOD_ID, name), item);
-        }
-        return block;
-    }
+	private static <B extends Block> B add(String name, B block, BlockItem item) {
+		add(name, block);
+		if (item != null) {
+			item.appendBlocks(Item.BLOCK_ITEMS, item);
+			ITEMS.put(new Identifier(Traverse.MOD_ID, name), item);
+		}
+		return block;
+	}
 
-    private static <B extends Block> B add(String name, B block) {
-        BLOCKS.put(new Identifier(Traverse.MOD_ID, name), block);
-        return block;
-    }
+	private static <B extends Block> B add(String name, B block) {
+		BLOCKS.put(new Identifier(Traverse.MOD_ID, name), block);
+		return block;
+	}
 
-    public static void register() {
-        for (Identifier id : ITEMS.keySet()) {
-            Registry.register(Registry.ITEM, id, ITEMS.get(id));
-        }
-        for (Identifier id : BLOCKS.keySet()) {
-            Registry.register(Registry.BLOCK, id, BLOCKS.get(id));
-        }
-    }
+	public static void register() {
+		for (Identifier id : ITEMS.keySet()) {
+			Registry.register(Registry.ITEM, id, ITEMS.get(id));
+		}
+		for (Identifier id : BLOCKS.keySet()) {
+			Registry.register(Registry.BLOCK, id, BLOCKS.get(id));
+		}
+	}
 
 }
--- src/main/java/io/github/terraformersmc/traverse/generation/TraverseGeneration.java
+++ src/main/java/io/github/terraformersmc/traverse/generation/TraverseGeneration.java
@@ -10,58 +10,58 @@ public class TraverseGeneration extends TraverseBiomes {
 
     public static void register() {
         // Arid Highlands
-        OverworldBiomes.addBaseBiome(ARID_HIGHLANDS, OverworldClimate.DRY, 2);
+        OverworldBiomes.addContinentalBiome(ARID_HIGHLANDS, OverworldClimate.DRY, 2);
 
         // Autumnal Wooded Hills
         OverworldBiomes.addHillsBiome(AUTUMNAL_WOODS, AUTUMNAL_WOODED_HILLS, 1);
 
         // Autumnal Woods
-        OverworldBiomes.addBaseBiome(AUTUMNAL_WOODS, OverworldClimate.TEMPERATE, 1);
-        OverworldBiomes.addBaseBiome(AUTUMNAL_WOODS, OverworldClimate.COOL, 0.25);
+        OverworldBiomes.addContinentalBiome(AUTUMNAL_WOODS, OverworldClimate.TEMPERATE, 1);
+        OverworldBiomes.addContinentalBiome(AUTUMNAL_WOODS, OverworldClimate.COOL, 0.25);
 
         // Cliffs
-        OverworldBiomes.addBaseBiome(CLIFFS, OverworldClimate.TEMPERATE, 0.4);
-        OverworldBiomes.addBaseBiome(CLIFFS, OverworldClimate.COOL, 0.6);
-        OverworldBiomes.addBaseBiome(CLIFFS, OverworldClimate.SNOWY, 0.8);
+        OverworldBiomes.addContinentalBiome(CLIFFS, OverworldClimate.TEMPERATE, 0.4);
+        OverworldBiomes.addContinentalBiome(CLIFFS, OverworldClimate.COOL, 0.6);
+        OverworldBiomes.addContinentalBiome(CLIFFS, OverworldClimate.SNOWY, 0.8);
 
         // Coniferous Forest
-        OverworldBiomes.addBaseBiome(CONIFEROUS_FOREST, OverworldClimate.TEMPERATE, 0.8);
-        OverworldBiomes.addBaseBiome(CONIFEROUS_FOREST, OverworldClimate.COOL, 0.3);
+        OverworldBiomes.addContinentalBiome(CONIFEROUS_FOREST, OverworldClimate.TEMPERATE, 0.8);
+        OverworldBiomes.addContinentalBiome(CONIFEROUS_FOREST, OverworldClimate.COOL, 0.3);
 
         // Coniferous Wooded Hills
         OverworldBiomes.addHillsBiome(CONIFEROUS_FOREST, CONIFEROUS_WOODED_HILLS, 1);
 
         // High Coniferous Forest
-        OverworldBiomes.addBaseBiome(HIGH_CONIFEROUS_FOREST, OverworldClimate.TEMPERATE, 0.2);
-        OverworldBiomes.addBaseBiome(HIGH_CONIFEROUS_FOREST, OverworldClimate.COOL, 0.075);
+        OverworldBiomes.addContinentalBiome(HIGH_CONIFEROUS_FOREST, OverworldClimate.TEMPERATE, 0.2);
+        OverworldBiomes.addContinentalBiome(HIGH_CONIFEROUS_FOREST, OverworldClimate.COOL, 0.075);
 
         // Rolling Hills
-        OverworldBiomes.addBaseBiome(ROLLING_HILLS, OverworldClimate.COOL, 0.7);
+        OverworldBiomes.addContinentalBiome(ROLLING_HILLS, OverworldClimate.COOL, 0.7);
 
         // Lush Swamp
         OverworldBiomes.addBiomeVariant(Biomes.SWAMP, LUSH_SWAMP, 0.2);
 
         // Meadow
-        OverworldBiomes.addBaseBiome(MEADOW, OverworldClimate.TEMPERATE, 0.9);
-        OverworldBiomes.addBaseBiome(MEADOW, OverworldClimate.COOL, 0.9);
+        OverworldBiomes.addContinentalBiome(MEADOW, OverworldClimate.TEMPERATE, 0.9);
+        OverworldBiomes.addContinentalBiome(MEADOW, OverworldClimate.COOL, 0.9);
         OverworldBiomes.addBiomeVariant(Biomes.PLAINS, MEADOW, 0.2, OverworldClimate.COOL, OverworldClimate.TEMPERATE);
 
         // Mini Jungle
-        OverworldBiomes.addBaseBiome(MINI_JUNGLE, OverworldClimate.TEMPERATE, 0.2);
+        OverworldBiomes.addContinentalBiome(MINI_JUNGLE, OverworldClimate.TEMPERATE, 0.2);
         OverworldBiomes.addBiomeVariant(Biomes.JUNGLE, MINI_JUNGLE, 0.15);
 
         // Snowy Coniferous Forest
-        OverworldBiomes.addBaseBiome(SNOWY_CONIFEROUS_FOREST, OverworldClimate.SNOWY, 0.5);
+        OverworldBiomes.addContinentalBiome(SNOWY_CONIFEROUS_FOREST, OverworldClimate.SNOWY, 0.5);
 
         // Snowy Coniferous Wooded Hills
         OverworldBiomes.addHillsBiome(SNOWY_CONIFEROUS_FOREST, SNOWY_CONIFEROUS_WOODED_HILLS, 1);
 
         // Snowy High Coniferous Forest
-        OverworldBiomes.addBaseBiome(SNOWY_HIGH_CONIFEROUS_FOREST, OverworldClimate.SNOWY, 0.125);
+        OverworldBiomes.addContinentalBiome(SNOWY_HIGH_CONIFEROUS_FOREST, OverworldClimate.SNOWY, 0.125);
 
         // Woodlands
-        OverworldBiomes.addBaseBiome(WOODLANDS, OverworldClimate.COOL, 1);
-        OverworldBiomes.addBaseBiome(WOODLANDS, OverworldClimate.TEMPERATE, 1);
+        OverworldBiomes.addContinentalBiome(WOODLANDS, OverworldClimate.COOL, 1);
+        OverworldBiomes.addContinentalBiome(WOODLANDS, OverworldClimate.TEMPERATE, 1);
 
         // Spawn Biomes
         FabricBiomes.addSpawnBiome(ARID_HIGHLANDS);
