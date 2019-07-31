4a809160117c9c5b4e08e511f78770db738be8bc
Prospector <prospectordev@gmail.com>
2019-07-31 16:49:05 -0700

    v1.7.6 - Fix generated yellow autumnal and brown trees not being as tall as they should

--- src/main/java/com/terraformersmc/traverse/feature/TraverseFeatures.java
+++ src/main/java/com/terraformersmc/traverse/feature/TraverseFeatures.java
@@ -18,9 +18,9 @@ public class TraverseFeatures {
 	private static final Map<Identifier, Feature<? extends FeatureConfig>> FEATURES = new HashMap<>();
 
 	public static final TerraformOakTreeFeature RED_AUTUMNAL_TREE = add("red_autumnal_tree", new TerraformOakTreeFeature(DefaultFeatureConfig::deserialize, false, Blocks.DARK_OAK_LOG.getDefaultState(), TraverseBlocks.RED_AUTUMNAL_LEAVES.getDefaultState()));
-	public static final TerraformOakTreeFeature BROWN_AUTUMNAL_TREE = add("brown_autumnal_tree", new TerraformOakTreeFeature(DefaultFeatureConfig::deserialize, false, Blocks.OAK_LOG.getDefaultState(), TraverseBlocks.BROWN_AUTUMNAL_LEAVES.getDefaultState()));
+	public static final TerraformOakTreeFeature BROWN_AUTUMNAL_TREE = add("brown_autumnal_tree", new TerraformOakTreeFeature(DefaultFeatureConfig::deserialize, false, 5, Blocks.OAK_LOG.getDefaultState(), TraverseBlocks.BROWN_AUTUMNAL_LEAVES.getDefaultState()));
 	public static final TerraformOakTreeFeature ORANGE_AUTUMNAL_TREE = add("orange_autumnal_tree", new TerraformOakTreeFeature(DefaultFeatureConfig::deserialize, false, Blocks.OAK_LOG.getDefaultState(), TraverseBlocks.ORANGE_AUTUMNAL_LEAVES.getDefaultState()));
-	public static final TerraformOakTreeFeature YELLOW_AUTUMNAL_TREE = add("yellow_autumnal_tree", new TerraformOakTreeFeature(DefaultFeatureConfig::deserialize, false, Blocks.BIRCH_LOG.getDefaultState(), TraverseBlocks.YELLOW_AUTUMNAL_LEAVES.getDefaultState()));
+	public static final TerraformOakTreeFeature YELLOW_AUTUMNAL_TREE = add("yellow_autumnal_tree", new TerraformOakTreeFeature(DefaultFeatureConfig::deserialize, false, 6, Blocks.BIRCH_LOG.getDefaultState(), TraverseBlocks.YELLOW_AUTUMNAL_LEAVES.getDefaultState()));
 	public static final MeadowFlowerFeature LUSH_FLOWER = add("lush_flower", new MeadowFlowerFeature(DefaultFeatureConfig::deserialize));
 	public static final OakTreeFeature MINI_JUNGLE_TREE = add("mini_jungle_tree", new OakTreeFeature(DefaultFeatureConfig::deserialize, false, 4, Blocks.JUNGLE_LOG.getDefaultState(), Blocks.JUNGLE_LEAVES.getDefaultState(), true));
 	public static final JungleGroundBushFeature OAK_SHRUB = add("oak_shrub", new JungleGroundBushFeature(DefaultFeatureConfig::deserialize, Blocks.OAK_LOG.getDefaultState(), Blocks.OAK_LEAVES.getDefaultState()));
