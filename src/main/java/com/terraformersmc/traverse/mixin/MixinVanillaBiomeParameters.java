03d8445b7357067c1eff873ac18efca4bf6fe674
gniftygnome <minecraft@gnifty.net>
2022-06-10 22:10:13 -0700

    Refactored the VanillaBiomeParameters option a bit.  I think this one may be workable.  Generation is not bad.  I haven't figured out how to load data biomes in time for this method yet though.

--- src/main/java/com/terraformersmc/traverse/mixin/MixinVanillaBiomeParameters.java
+++ src/main/java/com/terraformersmc/traverse/mixin/MixinVanillaBiomeParameters.java
@@ -1,9 +1,6 @@
 package com.terraformersmc.traverse.mixin;
 
 import com.terraformersmc.traverse.biome.TraverseBiomes;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.BuiltinRegistries;
-import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.BiomeKeys;
@@ -15,6 +12,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
+import java.util.Optional;
 import java.util.Random;
 
 @Mixin(VanillaBiomeParameters.class)
@@ -29,34 +27,64 @@ public abstract class MixinVanillaBiomeParameters {
 			locals = LocalCapture.NO_CAPTURE)
 	protected void traverse$overrideRegularBiome(int temperature, int humidity, MultiNoiseUtil.ParameterRange weirdness, CallbackInfoReturnable<RegistryKey<Biome>> cir) {
 		RegistryKey<Biome> biomeKey = cir.getReturnValue();
-		Biome biome = BuiltinRegistries.BIOME.get(biomeKey);
-		//Random random = new Random((long) humidity << 16 + temperature);
+
+		getBiomeOverride(temperature, humidity, biomeKey).ifPresent(cir::setReturnValue);
+	}
+
+	@Inject(at = @At(value = "RETURN"),
+			method = "getShoreBiome",
+			cancellable = true,
+			locals = LocalCapture.NO_CAPTURE)
+	protected void traverse$overrideShoreBiome(int temperature, int humidity, CallbackInfoReturnable<RegistryKey<Biome>> cir) {
+		RegistryKey<Biome> biomeKey = cir.getReturnValue();
+
+		getBiomeOverride(temperature, humidity, biomeKey).ifPresent(cir::setReturnValue);
+	}
+
+	@Inject(at = @At(value = "RETURN"),
+			method = "getBadlandsBiome",
+			cancellable = true,
+			locals = LocalCapture.NO_CAPTURE)
+	protected void traverse$overrideBadlandsBiome(int humidity, MultiNoiseUtil.ParameterRange weirdness, CallbackInfoReturnable<RegistryKey<Biome>> cir) {
+		RegistryKey<Biome> biomeKey = cir.getReturnValue();
+
+		getBiomeOverride(4, humidity, biomeKey).ifPresent(cir::setReturnValue);
+	}
+
+	@Inject(at = @At(value = "RETURN"),
+			method = "getNearMountainBiome",
+			cancellable = true,
+			locals = LocalCapture.NO_CAPTURE)
+	protected void traverse$overrideNearMountainBiome(int temperature, int humidity, MultiNoiseUtil.ParameterRange weirdness, CallbackInfoReturnable<RegistryKey<Biome>> cir) {
+		RegistryKey<Biome> biomeKey = cir.getReturnValue();
+
+		getBiomeOverride(temperature, humidity, biomeKey).ifPresent(cir::setReturnValue);
+	}
+
+	protected Optional<RegistryKey<Biome>> getBiomeOverride(int temperature, int humidity, RegistryKey<Biome> biomeKey) {
+		Optional<RegistryKey<Biome>> newBiomeKey = Optional.empty();
 
 		// 20% chance to replace vanilla biomes
-		double randomness = random.nextDouble();
-		System.out.println("TRAVERSE: Biome " + biomeKey.getValue() + " seen at T, H " + temperature + ", " + humidity + " yields chance: " + randomness);
-		if (biomeKey.getValue().getNamespace().equals("minecraft") && randomness < 0.2f) {
+		if (biomeKey.getValue().getNamespace().equals("minecraft") && random.nextDouble() < 0.2f) {
 			if (biomeKey.equals(BiomeKeys.FOREST)) {
-				cir.setReturnValue(RegistryKey.of(Registry.BIOME_KEY, new Identifier("traverse", "autumnal_woods")));
-			}
-			if (biomeKey.equals(BiomeKeys.TAIGA)) {
-				cir.setReturnValue(TraverseBiomes.CONIFEROUS_FOREST);
-			}
-			if (biomeKey.equals(BiomeKeys.BIRCH_FOREST)) {
-				cir.setReturnValue(TraverseBiomes.WOODLANDS);
-			}
-			if (biomeKey.equals(BiomeKeys.DESERT)) {
-				cir.setReturnValue(TraverseBiomes.DESERT_SHRUBLAND);
-			}
-			if (biomeKey.equals(BiomeKeys.SWAMP)) {
-				cir.setReturnValue(TraverseBiomes.LUSH_SWAMP);
-			}
-			if (biomeKey.equals(BiomeKeys.PLAINS)) {
-				cir.setReturnValue(TraverseBiomes.FLATLANDS);
-			}
-			if (biomeKey.equals(BiomeKeys.SNOWY_TAIGA)) {
-				cir.setReturnValue(TraverseBiomes.SNOWY_CONIFEROUS_FOREST);
+				newBiomeKey = Optional.of(TraverseBiomes.AUTUMNAL_WOODS);
+			} else if (biomeKey.equals(BiomeKeys.TAIGA)) {
+				newBiomeKey = Optional.of(TraverseBiomes.CONIFEROUS_FOREST);
+			} else if (biomeKey.equals(BiomeKeys.BIRCH_FOREST)) {
+				newBiomeKey = Optional.of(TraverseBiomes.WOODLANDS);
+			} else if (biomeKey.equals(BiomeKeys.DESERT)) {
+				newBiomeKey = Optional.of(TraverseBiomes.DESERT_SHRUBLAND);
+			} else if (biomeKey.equals(BiomeKeys.SWAMP) || (humidity == 4 && temperature > 2 && temperature < 5)) {
+				// 1.18.2, at least, does not give me a good place to hook in for SWAMP, so I'm winging it.
+				// I don't love this setting but it fits in OK, usually tucked along a jungle biome...
+				newBiomeKey = Optional.of(TraverseBiomes.LUSH_SWAMP);
+			} else if (biomeKey.equals(BiomeKeys.PLAINS)) {
+				newBiomeKey = Optional.of(TraverseBiomes.FLATLANDS);
+			} else if (biomeKey.equals(BiomeKeys.SNOWY_TAIGA)) {
+				newBiomeKey = Optional.of(TraverseBiomes.SNOWY_CONIFEROUS_FOREST);
 			}
 		}
+
+		return newBiomeKey;
 	}
 }
