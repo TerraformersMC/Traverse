0bd5b89115cb76fd146d49a9189dc3150eb11590
gniftygnome <minecraft@gnifty.net>
2022-06-17 15:58:14 -0700

    Data generation for biome tags and block loot providers.  No More Data.

--- common/src/main/java/com/terraformersmc/traverse/data/TraverseDatagen.java
+++ common/src/main/java/com/terraformersmc/traverse/data/TraverseDatagen.java
@@ -6,6 +6,8 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 public class TraverseDatagen implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator dataGenerator) {
+		dataGenerator.addProvider(TraverseBiomeTagProvider::new);
+		dataGenerator.addProvider(TraverseBlockLootTableProvider::new);
 		dataGenerator.addProvider(TraverseBlockTagProvider::new);
 		dataGenerator.addProvider(TraverseItemTagProvider::new);
 		dataGenerator.addProvider(TraverseRecipeProvider::new);
