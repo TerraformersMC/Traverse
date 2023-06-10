c24b4a9dbcccc037fd105d920849bc9f7f4512a5
gniftygnome <minecraft@gnifty.net>
2023-06-10 01:08:22 -0700

    Standardizing block and item tags and datagen some more.

--- common/src/main/java/com/terraformersmc/traverse/tag/TraverseBlockTags.java
+++ common/src/main/java/com/terraformersmc/traverse/tag/TraverseBlockTags.java
@@ -9,16 +9,17 @@ import net.minecraft.util.Identifier;
 
 public final class TraverseBlockTags {
 	public static final TagKey<Block> FIR_LOGS = TraverseBlockTags.of("fir_logs");
-	public static final TagKey<Block> PLANKS_THAT_BURN = TraverseBlockTags.of(new Identifier("c", "planks_that_burn"));
-	public static final TagKey<Block> STRIPPED_LOGS = TraverseBlockTags.of(new Identifier("c", "stripped_logs"));
-	public static final TagKey<Block> STRIPPED_WOOD = TraverseBlockTags.of(new Identifier("c", "stripped_wood"));
+	public static final TagKey<Block> PLANKS_THAT_BURN = TraverseBlockTags.of(Identifier.of("c", "planks_that_burn"));
+	public static final TagKey<Block> STRIPPED_LOGS = TraverseBlockTags.of(Identifier.of("c", "stripped_logs"));
+	public static final TagKey<Block> STRIPPED_WOOD = TraverseBlockTags.of(Identifier.of("c", "stripped_wood"));
 
+	@SuppressWarnings("UnnecessaryReturnStatement")
 	private TraverseBlockTags() {
 		return;
 	}
 
 	private static TagKey<Block> of(String path) {
-		return TraverseBlockTags.of(new Identifier(Traverse.MOD_ID, path));
+		return TraverseBlockTags.of(Identifier.of(Traverse.MOD_ID, path));
 	}
 
 	private static TagKey<Block> of(Identifier id) {
--- common/src/main/java/com/terraformersmc/traverse/tag/TraverseItemTags.java
+++ common/src/main/java/com/terraformersmc/traverse/tag/TraverseItemTags.java
@@ -8,16 +8,17 @@ import net.minecraft.util.Identifier;
 
 public final class TraverseItemTags {
 	public static final TagKey<Item> FIR_LOGS = TraverseItemTags.of("fir_logs");
-	public static final TagKey<Item> PLANKS_THAT_BURN = TraverseItemTags.of(new Identifier("c", "planks_that_burn"));
-	public static final TagKey<Item> STRIPPED_LOGS = TraverseItemTags.of(new Identifier("c", "stripped_logs"));
-	public static final TagKey<Item> STRIPPED_WOOD = TraverseItemTags.of(new Identifier("c", "stripped_wood"));
+	public static final TagKey<Item> PLANKS_THAT_BURN = TraverseItemTags.of(Identifier.of("c", "planks_that_burn"));
+	public static final TagKey<Item> STRIPPED_LOGS = TraverseItemTags.of(Identifier.of("c", "stripped_logs"));
+	public static final TagKey<Item> STRIPPED_WOOD = TraverseItemTags.of(Identifier.of("c", "stripped_wood"));
 
+	@SuppressWarnings("UnnecessaryReturnStatement")
 	private TraverseItemTags() {
 		return;
 	}
 
 	private static TagKey<Item> of(String path) {
-		return TraverseItemTags.of(new Identifier(Traverse.MOD_ID, path));
+		return TraverseItemTags.of(Identifier.of(Traverse.MOD_ID, path));
 	}
 
 	private static TagKey<Item> of(Identifier id) {
