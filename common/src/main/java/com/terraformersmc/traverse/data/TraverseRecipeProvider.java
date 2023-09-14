0e0a01f4c91c5a7251cb566f6da7b2ed780c4767
gniftygnome <minecraft@gnifty.net>
2023-09-13 19:52:36 -0700

    Update for Minecraft 1.20.2-pre4.
    - Update for Minecraft 1.20.2-pre4
    * Update Fabric API dep; closes #104

--- common/src/main/java/com/terraformersmc/traverse/data/TraverseRecipeProvider.java
+++ common/src/main/java/com/terraformersmc/traverse/data/TraverseRecipeProvider.java
@@ -7,13 +7,11 @@ import com.terraformersmc.traverse.tag.TraverseItemTags;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.advancement.criterion.InventoryChangedCriterion;
-import net.minecraft.data.server.recipe.RecipeJsonProvider;
+import net.minecraft.data.server.recipe.RecipeExporter;
 import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.recipe.book.RecipeCategory;
 import net.minecraft.util.Identifier;
-
-import java.util.function.Consumer;
 
 public class TraverseRecipeProvider extends FabricRecipeProvider {
 	protected TraverseRecipeProvider(FabricDataOutput dataOutput) {
@@ -21,7 +19,7 @@ public class TraverseRecipeProvider extends FabricRecipeProvider {
 	}
 
 	@Override
-	public void generate(Consumer<RecipeJsonProvider> exporter) {
+	public void generate(RecipeExporter exporter) {
 		offerBoatRecipe(exporter, TraverseBoatTypes.FIR_BOAT, TraverseBlocks.FIR_PLANKS);
 		offerChestBoatRecipe(exporter, TraverseBoatTypes.FIR_CHEST_BOAT, TraverseBoatTypes.FIR_BOAT);
 
