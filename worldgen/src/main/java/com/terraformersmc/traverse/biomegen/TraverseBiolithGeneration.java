736550e4343d7077ccaed47375bca4c47f6f40e6
gniftygnome <minecraft@gnifty.net>
2024-01-12 15:51:08 -0800

    Add biome generation in Modern Beta.
    * Update Biolith to 1.2.0-beta.1 for Modern Beta support
    - Allow Traverse biomes to generate in Modern Beta worlds

--- worldgen/src/main/java/com/terraformersmc/traverse/biomegen/TraverseBiolithGeneration.java
+++ worldgen/src/main/java/com/terraformersmc/traverse/biomegen/TraverseBiolithGeneration.java
@@ -1,6 +1,8 @@
 package com.terraformersmc.traverse.biomegen;
 
 import com.terraformersmc.biolith.api.biome.BiomePlacement;
+import com.terraformersmc.biolith.api.compat.BiolithCompats;
+import com.terraformersmc.biolith.api.compat.ModernBetaBiomeKeys;
 import com.terraformersmc.biolith.api.surface.SurfaceGeneration;
 import com.terraformersmc.traverse.Traverse;
 import com.terraformersmc.traverse.config.TraverseBiomeConfig;
@@ -21,6 +23,56 @@ public class TraverseBiolithGeneration implements Runnable {
 		if (BIOME_CONFIG.isBiomeEnabled(LUSH_SWAMP))               { BiomePlacement.replaceOverworld(BiomeKeys.SWAMP, LUSH_SWAMP, 0.2D); }
 		if (BIOME_CONFIG.isBiomeEnabled(SNOWY_CONIFEROUS_FOREST))  { BiomePlacement.replaceOverworld(BiomeKeys.SNOWY_TAIGA, SNOWY_CONIFEROUS_FOREST, 0.2D); }
 		if (BIOME_CONFIG.isBiomeEnabled(WOODLANDS))                { BiomePlacement.replaceOverworld(BiomeKeys.BIRCH_FOREST, WOODLANDS, 0.2D); }
+
+		// Target some Modern Beta biomes for replacement by ours.
+		if (BiolithCompats.isCompatEnabled(BiolithCompats.MODERN_BETA)) {
+			if (BIOME_CONFIG.isBiomeEnabled(AUTUMNAL_WOODS)) {
+				BiomePlacement.replaceOverworld(ModernBetaBiomeKeys.INFDEV_420, AUTUMNAL_WOODS, 0.3D);
+				BiomePlacement.replaceOverworld(ModernBetaBiomeKeys.INFDEV_611, AUTUMNAL_WOODS, 0.3D);
+				BiomePlacement.replaceOverworld(ModernBetaBiomeKeys.ALPHA, AUTUMNAL_WOODS, 0.4D);
+				BiomePlacement.replaceOverworld(ModernBetaBiomeKeys.BETA_SKY, AUTUMNAL_WOODS, 0.3D);
+				BiomePlacement.replaceOverworld(ModernBetaBiomeKeys.BETA_SEASONAL_FOREST, AUTUMNAL_WOODS, 0.2D);
+				BiomePlacement.replaceOverworld(ModernBetaBiomeKeys.PE_SEASONAL_FOREST, AUTUMNAL_WOODS, 0.2D);
+			}
+			if (BIOME_CONFIG.isBiomeEnabled(CONIFEROUS_FOREST)) {
+				BiomePlacement.replaceOverworld(ModernBetaBiomeKeys.INFDEV_611, CONIFEROUS_FOREST, 0.3D);
+				BiomePlacement.replaceOverworld(ModernBetaBiomeKeys.ALPHA, CONIFEROUS_FOREST, 0.4D);
+				BiomePlacement.replaceOverworld(ModernBetaBiomeKeys.BETA_TAIGA, CONIFEROUS_FOREST, 0.2D);
+				BiomePlacement.replaceOverworld(ModernBetaBiomeKeys.PE_TAIGA, CONIFEROUS_FOREST, 0.2D);
+			}
+			if (BIOME_CONFIG.isBiomeEnabled(DESERT_SHRUBLAND)) {
+				BiomePlacement.replaceOverworld(ModernBetaBiomeKeys.BETA_DESERT, DESERT_SHRUBLAND, 0.3D);
+				BiomePlacement.replaceOverworld(ModernBetaBiomeKeys.PE_DESERT, DESERT_SHRUBLAND, 0.3D);
+			}
+			if (BIOME_CONFIG.isBiomeEnabled(FLATLANDS)) {
+				BiomePlacement.replaceOverworld(ModernBetaBiomeKeys.INFDEV_227, FLATLANDS, 0.2D);
+				BiomePlacement.replaceOverworld(ModernBetaBiomeKeys.INFDEV_415, FLATLANDS, 0.2D);
+				BiomePlacement.replaceOverworld(ModernBetaBiomeKeys.INFDEV_420, FLATLANDS, 0.3D);
+				BiomePlacement.replaceOverworld(ModernBetaBiomeKeys.INFDEV_611, FLATLANDS, 0.3D);
+				BiomePlacement.replaceOverworld(ModernBetaBiomeKeys.ALPHA, FLATLANDS, 0.4D);
+				BiomePlacement.replaceOverworld(ModernBetaBiomeKeys.BETA_SKY, FLATLANDS, 0.3D);
+				BiomePlacement.replaceOverworld(ModernBetaBiomeKeys.BETA_PLAINS, FLATLANDS, 0.2D);
+				BiomePlacement.replaceOverworld(ModernBetaBiomeKeys.PE_PLAINS, FLATLANDS, 0.2D);
+			}
+			if (BIOME_CONFIG.isBiomeEnabled(LUSH_SWAMP)) {
+				BiomePlacement.replaceOverworld(ModernBetaBiomeKeys.BETA_SWAMPLAND, LUSH_SWAMP, 0.3D);
+				BiomePlacement.replaceOverworld(ModernBetaBiomeKeys.PE_SWAMPLAND, LUSH_SWAMP, 0.3D);
+			}
+			if (BIOME_CONFIG.isBiomeEnabled(SNOWY_CONIFEROUS_FOREST)) {
+				BiomePlacement.replaceOverworld(ModernBetaBiomeKeys.ALPHA_WINTER, SNOWY_CONIFEROUS_FOREST, 0.2D);
+				BiomePlacement.replaceOverworld(ModernBetaBiomeKeys.BETA_TUNDRA, SNOWY_CONIFEROUS_FOREST, 0.2D);
+				BiomePlacement.replaceOverworld(ModernBetaBiomeKeys.PE_TUNDRA, SNOWY_CONIFEROUS_FOREST, 0.2D);
+			}
+			if (BIOME_CONFIG.isBiomeEnabled(WOODLANDS)) {
+				BiomePlacement.replaceOverworld(ModernBetaBiomeKeys.INFDEV_415, WOODLANDS, 0.2D);
+				BiomePlacement.replaceOverworld(ModernBetaBiomeKeys.INFDEV_420, WOODLANDS, 0.3D);
+				BiomePlacement.replaceOverworld(ModernBetaBiomeKeys.INFDEV_611, WOODLANDS, 0.3D);
+				BiomePlacement.replaceOverworld(ModernBetaBiomeKeys.ALPHA, WOODLANDS, 0.4D);
+				BiomePlacement.replaceOverworld(ModernBetaBiomeKeys.BETA_SKY, WOODLANDS, 0.3D);
+				BiomePlacement.replaceOverworld(ModernBetaBiomeKeys.BETA_FOREST, WOODLANDS, 0.2D);
+				BiomePlacement.replaceOverworld(ModernBetaBiomeKeys.PE_FOREST, WOODLANDS, 0.2D);
+			}
+		}
 	}
 
 	// Use Biolith to register our Biome placements.
