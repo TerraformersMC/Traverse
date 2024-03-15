924b9712d7f51b2953d0f78c525b03492779be1c
gniftygnome <minecraft@gnifty.net>
2024-03-15 15:30:39 -0700

    Update to 24w11a.
    - Update to 24w11a

--- client/src/main/java/com/terraformersmc/traverse/TraverseClient.java
+++ client/src/main/java/com/terraformersmc/traverse/TraverseClient.java
@@ -1,13 +1,10 @@
 package com.terraformersmc.traverse;
 
 import com.terraformersmc.terraform.boat.api.client.TerraformBoatClientHelper;
-import com.terraformersmc.terraform.sign.SpriteIdentifierRegistry;
 import com.terraformersmc.traverse.block.TraverseBlocks;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.minecraft.client.render.RenderLayer;
-import net.minecraft.client.render.TexturedRenderLayers;
-import net.minecraft.client.util.SpriteIdentifier;
 import net.minecraft.util.Identifier;
 
 public class TraverseClient implements ClientModInitializer {
@@ -15,7 +12,6 @@ public class TraverseClient implements ClientModInitializer {
 	public void onInitializeClient() {
 		registerEntityRenderers();
 		registerRenderLayers();
-		registerTextures();
 	}
 
 	private static void registerEntityRenderers() {
@@ -27,10 +23,5 @@ public class TraverseClient implements ClientModInitializer {
 		BlockRenderLayerMap.INSTANCE.putBlocks(cutout, TraverseBlocks.FIR_SAPLING, TraverseBlocks.FIR_DOOR, TraverseBlocks.FIR_TRAPDOOR);
 		BlockRenderLayerMap.INSTANCE.putBlocks(cutout, TraverseBlocks.RED_AUTUMNAL_SAPLING, TraverseBlocks.BROWN_AUTUMNAL_SAPLING, TraverseBlocks.ORANGE_AUTUMNAL_SAPLING, TraverseBlocks.YELLOW_AUTUMNAL_SAPLING);
 		BlockRenderLayerMap.INSTANCE.putBlocks(cutout, TraverseBlocks.POTTED_RED_AUTUMNAL_SAPLING, TraverseBlocks.POTTED_BROWN_AUTUMNAL_SAPLING, TraverseBlocks.POTTED_ORANGE_AUTUMNAL_SAPLING, TraverseBlocks.POTTED_YELLOW_AUTUMNAL_SAPLING, TraverseBlocks.POTTED_FIR_SAPLING);
-	}
-
-	private static void registerTextures() {
-		SpriteIdentifierRegistry.INSTANCE.addIdentifier(new SpriteIdentifier(TexturedRenderLayers.SIGNS_ATLAS_TEXTURE, TraverseBlocks.FIR_SIGN.getTexture()));
-		SpriteIdentifierRegistry.INSTANCE.addIdentifier(new SpriteIdentifier(TexturedRenderLayers.SIGNS_ATLAS_TEXTURE, TraverseBlocks.FIR_HANGING_SIGN.getTexture()));
 	}
 }
